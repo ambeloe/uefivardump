c6d03277ef17d8550a7fd930a61afcb8532cfb59
nick <ambeloe@gmail.com>
2024-05-01 02:17:49 -0400

    fix help text

--- src/main.rs
+++ src/main.rs
@@ -114,7 +114,7 @@ unsafe fn main(image: Handle, mut st: SystemTable<Boot>) -> Status {
             Arg::Short('h') | Arg::Short('?') | Arg::Long("help") => {
                 println!(r"
                 uefivardumper takes three optional parameters: -v[true/false], -r,and -f.
-                    -v specifies if the saved variables are volatile or not (-vtrue only saves voltatile, -vfalse only saves volatile)
+                    -v specifies if the saved variables are volatile or not (-vtrue only saves voltatile, -vfalse only saves persistent)
                         not specifying the variable will save all variables.
                     -f specifies the output filename (path is relative to the drive uefivardumper is stored on) defaults to - for stdout
                     example: uefivardump.efi -vtrue -ftest.json
